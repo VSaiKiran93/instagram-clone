ba7484d1971375a7a6a49c79e53b5a5cbc28a137
VSaiKiran93 <venkatasaikiran.kureti@yahoo.com>
2023-12-10 22:41:26 -0500

    Initial commit

---
+++ src/timeline/Suggestions.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Avatar } from "@mui/material";
+import "./Suggestions.css";
+
+function Suggestions() {
+  return (
+    <div className="suggestions">
+      <div className="suggestions__title">Suggestions for you</div>
+      <div className="suggestions__usernames">
+
+        <div className="suggestion__username">
+          <div className='username__left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username__info'>
+              <span className='username'>sai_kiran93</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow__button'>Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className='username__left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username__info'>
+              <span className='username'>sai_kiran93</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow__button'>Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className='username__left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username__info'>
+              <span className='username'>sai_kiran93</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow__button'>Follow</button>
+        </div>
+
+        <div className="suggestion__username">
+          <div className='username__left'>
+            <span className='avatar'>
+              <Avatar>R</Avatar>
+            </span>
+            <div className='username__info'>
+              <span className='username'>sai_kiran93</span>
+              <span className='relation'>New to Instagram</span>
+            </div>
+          </div>
+          <button className='follow__button'>Follow</button>
+        </div>
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Suggestions
